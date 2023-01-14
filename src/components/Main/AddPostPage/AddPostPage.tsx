b20b6ce189cd0b7bd9c94e0ae23b64b9fe78df6c
erynder-z <stfn.bgr@gmail.com>
2023-01-14 18:41:28 +0100

    Handle imag eupload in tinyMCE

--- src/components/Main/AddPostPage/AddPostPage.tsx
+++ src/components/Main/AddPostPage/AddPostPage.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useEffect, useRef, useState } from 'react';
+import React, { ChangeEvent, ReactNode, useEffect, useRef, useState } from 'react';
 import { Editor } from '@tinymce/tinymce-react';
 import { Editor as TinyMCEEditor } from 'tinymce';
 import './AddPostPage.css';
@@ -146,9 +146,53 @@ export default function AddPostPage({ token }: Props) {
                   toolbar:
                     'undo redo | blocks | ' +
                     'bold italic forecolor | alignleft aligncenter ' +
-                    'alignright alignjustify | bullist numlist outdent indent | ' +
+                    'alignright alignjustify | image | bullist numlist outdent indent | ' +
                     'removeformat | help',
-                  content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+                  content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+                  file_picker_callback: (cb, value, meta) => {
+                    const createFileInput = () => {
+                      const input = document.createElement('input');
+                      input.setAttribute('type', 'file');
+                      input.setAttribute('accept', 'image/*');
+                      return input;
+                    };
+
+                    const handleFileSelected =
+                      (cb: (value: string, meta?: Record<string, any> | undefined) => void) =>
+                      (event: Event) => {
+                        if (event.target && event.target.files) {
+                          const file = event.target?.files[0];
+                          const reader = new FileReader();
+                          reader.onload = handleFileRead(cb, file);
+                          reader.readAsDataURL(file);
+                        }
+                      };
+
+                    const handleFileRead =
+                      (
+                        cb: (value: string, meta?: Record<string, any> | undefined) => void,
+                        file: File
+                      ) =>
+                      (event: ProgressEvent<FileReader>) => {
+                        if (event.target) {
+                          const base64 = (event.target.result as string)?.split(',')[1];
+
+                          const blobCache = tinymce.activeEditor?.editorUpload.blobCache;
+                          const id = createBlobId();
+                          const blobInfo = blobCache.create(id, file, base64);
+
+                          blobCache.add(blobInfo);
+                          cb(blobInfo.blobUri(), { title: file.name });
+                        }
+                      };
+
+                    const createBlobId = () => {
+                      return 'blobid' + new Date().getTime();
+                    };
+                    const fileInput = createFileInput();
+                    fileInput.onchange = handleFileSelected(cb);
+                    fileInput.click();
+                  }
                 }}
               />
             </div>
