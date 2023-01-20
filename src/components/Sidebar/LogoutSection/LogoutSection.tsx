963c9888176c7d0dfb070984f2f7ec6d2b3eb6bc
erynder-z <stfn.bgr@gmail.com>
2023-01-20 18:22:24 +0100

    Update logout logic

--- src/components/Sidebar/LogoutSection/LogoutSection.tsx
+++ src/components/Sidebar/LogoutSection/LogoutSection.tsx
@@ -3,13 +3,17 @@ import './LogoutSection.css';
 import { FaPowerOff } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 import AuthContext from '../../../contexts/AuthContext';
+import CurrentViewContext from '../../../contexts/CurrentViewContext';
 
 export default function LogoutSection() {
   const { setToken, setUser } = useContext(AuthContext);
+  const { setCurrentView } = useContext(CurrentViewContext);
+
   const navigate = useNavigate();
 
   const handleLogout = () => {
-    localStorage.removeItem('jwt');
+    window.localStorage.clear();
+    setCurrentView('All');
     setToken(null);
     setUser(null);
     navigate('/');
