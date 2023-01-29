1b1e632260dfc10ec4e72531b1db0486b3854296
erynder-z <stfn.bgr@gmail.com>
2023-01-29 21:02:15 +0100

    Add theme

--- src/interfaces/customTypes.ts
+++ src/interfaces/customTypes.ts
@@ -1,3 +1,3 @@
 export type ViewType = 'All' | 'Published' | 'Unpublished' | 'Manual' | 'Other';
 
-export type ThemeType = 'Default' | 'Matte' | 'Dark' | 'Bright';
+export type ThemeType = 'Default' | 'Matte' | 'Dark' | 'Bright' | 'Chameleon';
